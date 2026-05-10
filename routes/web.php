898810df309f99f9c9138b71c614ef72cd664de4
chokorimu <rezondavar@gmail.com>
2026-05-10 17:31:23 +0700

    ssebelum merge

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-Route::get('/', function () { return redirect('/login'); });
-
-Route::get('/pinjam', function () { return view('pinjam'); });
-Route::get('/jadwal', function () { return view('jadwal'); });
-Route::get('/pinjam/form', function () { return view('form_pinjam'); });
 use App\Http\Controllers\RuanganController;
 use App\Http\Controllers\PeminjamanController;
 
@@ -15,13 +9,10 @@ Route::get('/', function () {
     return redirect('/home');
 });
 
-Route::get('/login', [AuthController::class, 'showLogin']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/jadwal', function () {
+    return view('jadwal');
+});
 
-Route::get('/register', [AuthController::class, 'showRegister']);
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::get('/logout', [AuthController::class, 'logout']);
 Route::get('/home', [RuanganController::class, 'index'])->name('ruangan.index');
 
 Route::get('/ruangan/tambah', [RuanganController::class, 'create'])->name('ruangan.create');
@@ -30,3 +21,9 @@ Route::get('/ruangan/pinjam/{id}', [RuanganController::class, 'pinjamForm'])->na
 
 Route::get('/pinjam/form/{id}', [PeminjamanController::class, 'create'])->name('pinjam.create');
 Route::post('/pinjam/simpan', [PeminjamanController::class, 'store'])->name('pinjam.store');
+
+Route::get('/login', [AuthController::class, 'showLogin']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegister']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/logout', [AuthController::class, 'logout']);
